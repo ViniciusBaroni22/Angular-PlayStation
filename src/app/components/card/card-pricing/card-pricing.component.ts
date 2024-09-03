a88bc5952066ea615ed07e846ad23ef62859334f
ViniciusBaroni22 <baronivinicius_2201@hotmail.com>
2024-09-03 13:56:43 -0300

    project created

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent implements OnInit {
+
+  @Input()
+	gameType:string ="Digital PS4"
+	@Input()
+	gamePrice:string="R$ 299,99"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
